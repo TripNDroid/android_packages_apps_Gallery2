e18b2688e3d89e859081b3748ee4b3240b9b006c
TripNRaVeR <noerialexeew@live.nl>
2013-10-27 22:45:50 +0100

    use yuv420sp-adreno preview format for htc devices

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -2019,6 +2019,7 @@ public class PhotoModule
           if (Util.useHTCCamMode()) {
             mParameters.set("zsl", "on");
             mParameters.set("skinToneEnhancement", "enable");
+            mParameters.set("preview-format", "yuv420sp-adreno");
           } else {
             mParameters.set("camera-mode", "1");
           }
